493e95356f218fb9d0c5e8b19917592a2b278882
Catalín Munteanu <catalinapampin@gmail.com>
2023-06-30 16:05:12 -0300

    update in footer

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 <footer>
     <article class="datos-contacto">
-        <img src="img/icono-bloemen.png">
-        <p class="uppercase-text">Química Bloemendaal SRL</p>
-        <p class="uppercase-text">Juan B. Justo 5450</p>
-        <p>Mar del Plata (7600) - Buenos Aires</p>
-        <p>WhatsApp: 223 528 4000</p>
+        <img src="img/footer-logo.png">
     </article>
     <article class="redes-footer">
         <a href="https://www.facebook.com/profile.php?id=100088179257884&mibextid=ZbWKwL" target="_blank"><img src="img/icono-fb.png"></a>
--- index.php
+++ index.php
@@ -116,18 +116,16 @@ function showSlides() {
                     <label>Consulta</label><br>
                     <textarea name="consulta" class="consultaForm" rows="4" cols="22" maxlength="3000" required></textarea>
                 </form>
+                <div class="footer-index">
                 <article class="footerInsertado">
-                    <img src="img/icono-bloemen.png">
-                    <p class="uppercase-text">Química Bloemendaal SRL</p>
-                    <p class="uppercase-text">Juan B. Justo 5450</p>
-                    <p>Mar del Plata (7600) - Buenos Aires</p>
-                    <p>WhatsApp: 223 528 4000</p>
+                    <img src="img/footer-logo.png">
                 </article>
                 <article class="iconosContacto">
                     <a href="https://www.facebook.com/profile.php?id=100088179257884&mibextid=ZbWKwL" target="_blank"><img src="img/icono-fb.png"></a>
                     <a href="https://www.linkedin.com/in/fertilizantes-bloemen-a4a4b884" target="_blank"><img src="img/icono-linkedin.png"></a>
                     <a href="https://instagram.com/bloemen_fertilizantes" target="_blank"><img src="img/icono-ig.png"></a>
                 </article>
+                </div>
             </div>
         </section>
     </main>
