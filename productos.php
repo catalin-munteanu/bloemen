804001b29cba69ae888d6327bb6fdb31766bbdee
Catalín Munteanu <catalinapampin@gmail.com>
2023-07-09 13:33:56 -0300

    update productos

--- productos.php
+++ productos.php
@@ -15,7 +15,7 @@
 
 <body>
 
-<?php require_once 'header.php'; ?>
+    <?php require_once 'header.php'; ?>
 
     <main>
 
@@ -26,14 +26,65 @@
         </section>
 
         <section class="detalle-productos">
-            <article class="productos-foliar">
-                <figure>
-                    <img class="portadas-productos" src="img/productos-foliar-sintexto.png">
-                </figure>
-            </article>
+            <details>
+                <summary>
+                    <img class="portadas-productos" src="img/productos-foliar-mobile.png">
+                </summary>
+                <article class="productos-foliar">
+                    <ul>
+                        <li>
+                            <details>
+                                <summary class="uppercase-text">Aminocorrectores</summary>
+                                <p class="descripcion-productos">Son correctores foliares a base de Aminoácidos más el elemento corrector con el agregado de micronutrientes.
+                                    Actúan en el crecimiento, activan la fotosíntesis y mejoran la calidad y el rendimiento de los cultivos.</p>
+                                <div class="lista-productos-foliar">
+                                    <img src="img/prod-foliar AMINO-Fe.png"><br>
+                                    <img src="img/prod-foliar-AMINO-K.png"><br>
+                                    <img src="img/prod-foliar-Amino-ZN_MN.png"><br>
+                                    <img src="img/prod-foliar-BIOBLOEMEN.png"><br>
+                                    <img src="img/prod-foliar-CORRECTOR-AMINO-B.png"><br>
+                                    <img src="img/prod-foliar-CORRECTOR-AMINO-Mg.png"><br>
+                                    <img src="img/prod-foliar-CORRECTOR-AMINO-Zn.png"><br>
+                                    <img src="img/prod-foliar-CORRECTOR-DE-Ca-BIOBLOEMEN.png"><br>
+                                    <img src="img/prod-foliar-CORRECTOR-DE-N-BIOBLOEMEN.png"><br>
+                                    <img src="img/prod-foliar-VERDIGER.png"><br>
+                                </div>
+                            </details>
+                        </li>
+                        <li>
+                            <details>
+                                <summary class="uppercase-text">Maxicorrectores</summary>
+                                <p class="descripcion-productos">Son correctores foliares de elevada concentración de nutriente corrector, mejoran el cuajado, la floración
+                                    y la calidad de los cultivos.</p>
+                                <div class="lista-productos-foliar">
+                                    <img src="img/prod-maxi-foliar-MAXIBORO.png"><br>
+                                    <img src="img/prod-maxi-foliar-MAXICALCIO.png"><br>
+                                </div>
+                            </details>
+                        </li>
+                        <li>
+                            <details>
+                                <summary class="uppercase-text">Fosfitos</summary>
+                                <p class="descripcion-productos">Son bioestimulantes con triple acción estimulante.<br>
+                                    – Incrementa las defensas naturales de las plantas.<br>
+                                    – Reduce las situaciones de estrés.<br>
+                                    – Incorpora alto contenido de nutrientes.</p>
+                                <div class="lista-productos-foliar">
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE Ca.png"><br>
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE Cu.png"><br>
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE Mg.png"><br>
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE Mn.png"><br>
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE POTASIO.png"><br>
+                                    <img src="img/prod-fosfito-foliar-FOSFITO DE Zn.png"><br>
+                                </div>
+                            </details>
+                        </li>
+                    </ul>
+                </article>
+            </details>
             <article class="productos-suelo">
                 <figure>
-                    <img class="portadas-productos" src="img/productos-suelo-sintexto.png">
+                    <img class="portadas-productos" src="img/productos-suelo-mobile.png">
                 </figure>
             </article>
         </section>
