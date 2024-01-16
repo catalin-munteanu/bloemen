c5268ad39cc33ff5d04b1fe922282f343e1ad9b9
Catalín Munteanu <catalinapampin@gmail.com>
2024-01-16 11:00:16 -0300

    updated images in slider and google scripts

--- index.php
+++ index.php
@@ -45,16 +45,39 @@ if (
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-5KY9W14R2J"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
+
     function gtag() {
       dataLayer.push(arguments);
     }
     gtag('js', new Date());
     gtag('config', 'G-5KY9W14R2J');
   </script>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-KT9WGR52');
+  </script>
+  <!-- End Google Tag Manager -->
   <title>Bloemen - Fertilizantes Sustentables</title>
 </head>
 
 <body>
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT9WGR52" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   <?php require_once 'header.php'; ?>
 
